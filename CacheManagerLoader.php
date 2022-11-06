e344abc4fef2398d3203230632db0f735f11dbb5
Caio Marcatti <caio.marcatti@rb.com.br>
2022-11-06 09:42:53 -0300

    feature launcher

---
+++ CacheManagerLoader.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace CaioMarcatti12\Event;
+
+use CaioMarcatti12\CacheManager\Interfaces\CacheInterface;
+use CaioMarcatti12\Core\Bean\Objects\BeanProxy;
+use CaioMarcatti12\Core\Launcher\Annotation\Launcher;
+use CaioMarcatti12\Core\Launcher\Enum\LauncherPriorityEnum;
+use CaioMarcatti12\Core\Launcher\Interfaces\LauncherInterface;
+
+#[Launcher(LauncherPriorityEnum::BEFORE_LOAD_APPLICATION)]
+class CacheManagerLoader implements LauncherInterface
+{
+    public function handler(): void
+    {
+        BeanProxy::add(CacheInterface::class, RedisCacheAdapter::class);
+
+    }
+}
